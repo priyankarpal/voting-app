25e3300ff0d046b92b728241a20394564d8ffe65
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2024-04-24 21:30:09 +0530

    fix: change host & port

--- src/redis/client.js
+++ src/redis/client.js
@@ -2,8 +2,8 @@ import "dotenv/config";
 import { Redis } from "ioredis";
 
 const client=new Redis({
-    host: "localhost",
-    port: 6379,
+    host: 'redis-18304.c305.ap-south-1-1.ec2.redns.redis-cloud.com',
+    port: 18304,
     password: process.env.REDIS_PASSWORD
 });
 
