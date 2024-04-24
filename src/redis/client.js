701778d5f010d7816ae943b98c61d8c50b62e431
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2024-04-24 21:25:04 +0530

    feat: added redis password etc

--- src/redis/client.js
+++ src/redis/client.js
@@ -1,6 +1,11 @@
+import "dotenv/config";
 import { Redis } from "ioredis";
 
-const client=new Redis();
+const client=new Redis({
+    host: "localhost",
+    port: 6379,
+    password: process.env.REDIS_PASSWORD
+});
 
 
 export default client;
